bdabd48b8b6e2a3fc188d98c8c8e24c77c868f5b
NguyetAnh0206 <phamnguyetanh0206@gmail.com>
2026-08-13 22:55:20 +0700

    ptna.ltwlession01

---
+++ ptna.day01/ptna.day01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ptna.day01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ptna.ltwlession01/ptna.ltwlession01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ptna.ltwlession01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to AspNetCore MVC - UTC.K65CNT2.LTW");
+        }
+    }
+}
