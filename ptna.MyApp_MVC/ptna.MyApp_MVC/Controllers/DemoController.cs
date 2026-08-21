fbe9407112eb6dfbc4378104ee9f225e65807e0a
NguyetAnh0206 <phamnguyetanh0206@gmail.com>
2026-08-21 23:16:27 +0700

    ptna.MyApp_MVC

---
+++ ptna.MyApp_MVC/ptna.MyApp_MVC/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ptna.MyApp_MVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
