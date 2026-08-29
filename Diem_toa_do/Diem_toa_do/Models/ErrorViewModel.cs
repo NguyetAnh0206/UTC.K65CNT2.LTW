6f379266b074dffe37073e9a6414c83ff73c3fb6
NguyetAnh0206 <phamnguyetanh0206@gmail.com>
2026-08-29 23:52:09 +0700

    Diem_toa_do

--- Diem_toa_do/Diem_toa_do/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Diem_toa_do.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
